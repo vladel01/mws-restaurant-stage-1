afab9abcb367c012cd1913a957e12d8a743dee83
Alex Vlad <vlad@avanticart.ro>
2018-05-10 23:42:13 +0300

    refactor: dbhelper fetch restaurants from server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,28 +7,55 @@ class DBHelper {
         * Database URL.
         * Change this to restaurants.json file location on your server.
     */
-    static get DATABASE_URL() {
-        const port = 8000 // Change this to your server port
-        return `http://localhost:${port}/data/restaurants.json`;
-    }
+    // static get DATABASE_URL() {
+    //     const port = 1337 // Change this to your server port
+    //     return `http://localhost:${port}/data/restaurants`;
+    // }
 
     /**
         * Fetch all restaurants.
         */
+    // static fetchRestaurants(callback) {
+    //     let xhr = new XMLHttpRequest();
+    //         xhr.open('GET', DBHelper.DATABASE_URL);
+    //         xhr.onload = () => {
+    //             if (xhr.status === 200) { // Got a success response from server!
+    //                 const json = JSON.parse(xhr.responseText);
+    //                 const restaurants = json.restaurants;
+    //                 callback(null, restaurants);
+    //             } else { // Oops!. Got an error from server.
+    //                 const error = (`Request failed. Returned status of ${xhr.status}`);
+    //                 callback(error, null);
+    //             }
+    //         };
+    //     xhr.send();
+    // }
+
+
     static fetchRestaurants(callback) {
-        let xhr = new XMLHttpRequest();
-            xhr.open('GET', DBHelper.DATABASE_URL);
-            xhr.onload = () => {
-                if (xhr.status === 200) { // Got a success response from server!
-                    const json = JSON.parse(xhr.responseText);
-                    const restaurants = json.restaurants;
-                    callback(null, restaurants);
-                } else { // Oops!. Got an error from server.
-                    const error = (`Request failed. Returned status of ${xhr.status}`);
-                    callback(error, null);
+        fetch('http://localhost:1337/restaurants', {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function(response) {
+                if (response.status !== 200) {
+                    console.log('Request failed. Returned status of ' + response.status);
+                    return;
                 }
-            };
-        xhr.send();
+                //if (response) {
+                    response.json().then(function(data) {
+                        return callback(null, data);
+                    });
+
+                //console.log(response.json());
+                //} else {
+                //    callback('error', null);
+                //}
+            }).catch(function() {
+                callback('errror', null);
+            });
+
+
     }
 
     /**
