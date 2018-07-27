0d4c40f4830f3c80465a0cbe0186e5afcce4c157
Alex Vlad <vlad@avanticart.ro>
2018-07-28 00:36:38 +0300

    style: title and breadcrumb

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('js/idbController.js');
 
 console.log('sevice worker present');
 
-var myCacheNames = 'mws-restaurant-v53';
+var myCacheNames = 'mws-restaurant-v54';
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
